5255ca14dcec817b0a8b29e999837fe07ee67407
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-03-08 15:56:31 -0800

    minor changes

--- HW2/HW2Code.py
+++ HW2/HW2Code.py
@@ -5,7 +5,7 @@ from numpy import linalg
 
 def main():
     data = parser()
-    table = testTable(data)
+    table = createTable(data)
     centeredTable = centeredCosine(table)
     similarity = computeSimilarity(centeredTable)
     print(table)
@@ -15,7 +15,7 @@ def parser():
         data = file.readlines()
     return data[1:]
 
-def testTable(data):
+def createTable(data):
     movie_ids = []
     user_ids = []
     ratingDict = {}
@@ -48,57 +48,6 @@ def testTable(data):
     
     return table
 
-def createTable(data):
-    userHigh = 0
-    movieHigh = 0
-
-
-    # [id1, id2] : value
-
-
-    ratingsDict = {} # [ [userId, movieId] : rating]
-    movieDict = {} # [movieId : column] 
-    userDict = {} # [userId : row]
-    table = np.array([])
-    curRow = 0
-    curCol = 0
-
-    for row in data:
-        line = row.split(",")
-
-        user = int(line[0])
-        movie = int(line[1])
-        rating = float(line[2])
-
-        ratingsDict[user, movie] = rating
-        if user not in userDict:   
-            userDict[user] = curRow
-            curRow += 1
-        
-        if movie not in movieDict:
-            movieDict[movie] = curCol
-            curCol+=1
-    table = csr_matrix(curRow, curCol)
-
-
-
-
-
-    # for row in data:
-    #     line = row.split(",")
-    #     user = int(line[0])
-    #     movie = int(line[1])
-
-    #     if user > userHigh:
-    #         userHigh = user
-    #     if movie > movieHigh:
-    #         movieHigh = movie
-    # table = np.zeros((userHigh, movieHigh), dtype=float)
-    # movieSet = movieParser(data)
-
-
-    return table
-
 
 def movieParser(data):
     movie_ids = []
@@ -116,27 +65,37 @@ def rowMean(row):
     return np.subtract(row, mean)
 
 def computeSimilarity(table):
+
     similarity_scores = np.zeros((table.shape[1], table.shape[1]))
 
-
-
-    # combos = combinations([i for i in range(table.shape[1])], 2)
-
+    # combos = list(combinations([i for i in range(table.shape[1])], 2))
 
     # for pair in combos:
 
-    #     vector_1 = table[:,pair[0]]
-    #     vector_2 = table[:,pair[1]]
+    #     list1 = table[:,pair[0]]
+    #     list2 = table[:,pair[1]]
+    #     similarity_scores[pair] = list1.dot(list2)/ (np.linalg.norm(list1) * np.linalg.norm(list2))
 
-    #     similarity_score[pair] = np.dot(vector_1, vector_2)/(linalg.norm(vector_1)*linalg.norm(vector_2))
-
-    for i in range(table.shape[1]):
-        for j in range(table.shape[1]):
-            similarity_scores[i][j] = np.dot(table[:,i], table[:,j])/(linalg.norm(table[:,i])*linalg.norm(table[:,j]))
+    # return similarity_scores
 
 
 
-    return similarity_score
+    #similarity_scores = {}
+
+
+    # testing for only the first 50 movies
+    combos = combinations([i for i in range(50)], 2)
+
+
+    for pair in combos:
+
+        vector_1 = table[:,pair[0]]
+        vector_2 = table[:,pair[1]]
+
+        similarity_scores[pair] = np.dot(vector_1, vector_2)/(linalg.norm(vector_1)*linalg.norm(vector_2))
+    
+    return similarity_scores
+
 
 if __name__ == "__main__":
     main()
