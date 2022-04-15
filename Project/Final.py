38b59ff87fa370524470b4ac3f864d2cbc8b4dde
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-04-15 16:06:49 -0700

    created customer : item dict

--- Project/Final.py
+++ Project/Final.py
@@ -2,12 +2,30 @@ import numpy as np
 import pandas as pd
 
 def main():
-    data = pd.read_csv('Groceries_dataset.csv')
-    allProducts = data['itemDescription'].value_counts()
-    print(allProducts)
+    df = pd.read_csv('Groceries_dataset.csv')
+    data = preprocess(df)
 
 
 
+    
+def preprocess(df):
+    data = {}
+    grouped = df.groupby(['Member_number', 'itemDescription'])
+    print(grouped.first())
+    for name, group in grouped['Member_number']:
+        key = name[0]
+        value = name[1]
+        print(key)
+        print(value)
+        if key in data.keys():
+            data[key].append(value)
+        else:
+             data[key] = [value]
+    return data
+
+
+
+    
 
 if __name__ == "__main__":
     main()
